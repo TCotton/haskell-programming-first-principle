63fea5a5999565fcc81db68fe3bee747a65c5468
Andy Walpole <andy.walpole@gmail.com>
2021-02-16 20:53:58 +0000

    added triple function

--- test.hs
+++ test.hs
@@ -1,2 +1,5 @@
-sayHello :: [Char] -> IO ()
-sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+--sayHello :: [Char] -> IO ()
+--sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+triple :: Num a => a -> a
+triple x = x * 3
