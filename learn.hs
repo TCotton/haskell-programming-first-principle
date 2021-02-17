60ac8f2bf01edda7f6260b601d407a848110917d
Andy Walpole <andy.walpole@gmail.com>
2021-02-17 13:01:41 +0000

    added learn.hs file

---
+++ learn.hs
@@ -0,0 +1,23 @@
+module Learn where
+    x = 10 * 5 + y
+
+    myResult = x * 5
+
+    y = 10
+
+    area x = 3.14 * (x * x)
+
+    double x = x * 2
+
+    z = 7
+    t = 10
+    f = x + y
+
+
+
+
+
+
+
+
+
--- test.hs
+++ test.hs
@@ -1,5 +1,15 @@
---sayHello :: [Char] -> IO ()
---sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+module Main where
+sayHello :: [Char] -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
 
 triple :: Num a => a -> a
 triple x = x * 3
+
+half x = x / 2
+square x = x * x
+
+pifind :: Floating a => a -> a -> a
+pifind x y = pi * x * y
+
+--main :: IO ()
+main = putStr "Hello, World!"
